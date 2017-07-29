13a1a0f7b059b1618219bf979eb8803eab2a7b00
themostretarded <marcosresidentevil@hotmail.com>
2017-07-28 19:11:31 -0500

    cambios de la pagina

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
-  render() {
+  render()
+    {
     return (
       <div className="app flex-row align-items-center">
         <div className="container">
@@ -10,22 +11,31 @@ class Login extends Component {
               <div className="card-group mb-0">
                 <div className="card p-4">
                   <div className="card-block">
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
+                      
+                    <h1>Iniciar</h1>                      
+                    <p className="text-muted">Registrar cuenta</p>
+                      
                     <div className="input-group mb-3">
-                      <span className="input-group-addon"><i className="icon-user"></i></span>
-                      <input type="text" className="form-control" placeholder="Username"/>
+                        <span className="input-group-addon">
+                            <i className="icon-user"></i>
+                        </span>
+                        <input type="text" className="form-control" placeholder="Usuario"/>
                     </div>
+                      
                     <div className="input-group mb-4">
-                      <span className="input-group-addon"><i className="icon-lock"></i></span>
-                      <input type="password" className="form-control" placeholder="Password"/>
+                        <span className="input-group-addon">
+                            <i className="icon-lock"></i>
+                        </span>
+                        <input type="password" className="form-control" placeholder="Contraseña"/>
                     </div>
+                      
                     <div className="row">
                       <div className="col-6">
-                        <button type="button" className="btn btn-primary px-4">Login</button>
+                          <button type="button" className="btn btn-primary px-4">Listo</button>
                       </div>
+                        
                       <div className="col-6 text-right">
-                        <button type="button" className="btn btn-link px-0">Forgot password?</button>
+                        <button type="button" className="btn btn-link px-0">Ya se te olvido?</button>
                       </div>
                     </div>
                   </div>
@@ -33,9 +43,9 @@ class Login extends Component {
                 <div className="card card-inverse card-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
                   <div className="card-block text-center">
                     <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                      <button type="button" className="btn btn-primary active mt-3">Register Now!</button>
+                      <h2>Iniciar ya</h2>
+                      <p>En caso de tener problemas favor de contactar a su coordinador</p>
+                      <button type="button" className="btn btn-primary active mt-3">Registrate ahora!</button>
                     </div>
                   </div>
                 </div>
